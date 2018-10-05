79646781c640513d88bbbb26d0f3be951cdd29d2
YoshidaKazuki <yoshidakazuki@team-lab.com>
2018-10-06 01:19:07 +0900

    step5 done. add configuration

--- main.go
+++ main.go
@@ -3,13 +3,19 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
   "github.com/kazuki-yoshida-0602/kubernetes-ready-go-service/handlers"
 )
 
 func main() {
 	log.Print("Starting the service...")
+
+  port := os.Getenv("PORT")
+  if port == "" {
+    log.Fatal("Port is not set.")
+  }
   router := handlers.Router()
 	log.Print("The service is ready to listen and serve.")
-	log.Fatal(http.ListenAndServe(":8000", router))
+	log.Fatal(http.ListenAndServe(":"+port, router))
 }
